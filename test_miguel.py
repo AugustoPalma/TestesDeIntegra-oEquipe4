4278414ec4abd00940f914cae0c487c61d066664
Miguel Drozino <160974232+MDrozino@users.noreply.github.com>
2025-10-23 21:28:50 -0300

    [TDD Red]
    parametros inválidos - contagem acervo correta - interface acesso do repositorio

---
+++ test_miguel.py
@@ -0,0 +1,20 @@
+import unittest
+# from .relatorio_service import RelatorioService 
+
+class TestMiguelRelatorios(unittest.TestCase):
+
+    def setUp(self):
+        # self.service = RelatorioService()
+        pass
+
+    def test_lanca_excecao_parametros_invalidos(self):
+        self.fail("Teste unitário 9 - Pendente de implementação (TDD RED)")
+
+    def test_contagem_status_acervo_correta(self):
+        self.fail("Teste unitário 10 - Pendente de implementação (TDD RED)")
+
+    def test_contrato_interface_acesso_repositorio(self):
+        self.fail("Teste de Contrato 5 - Pendente de implementação (TDD RED)")
+
+if __name__ == '__main__':
+    unittest.main()
